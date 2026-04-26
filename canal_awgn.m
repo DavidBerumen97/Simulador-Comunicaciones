d6c22b982004c02fa94ab0de794c3f623af45cb7
beruman97 <beruman97@gmail.com>
2026-04-25 19:37:14 -0600

    Mejor codigo para agregar ruido

--- canal_awgn.m
+++ canal_awgn.m
@@ -1,5 +1,6 @@
 function senial_ruido = canal_awgn(mensaje,snr_db)
-    potencia_ruido = (sum(abs(mensaje).^2)/length(mensaje))/(10^(snr_db/10));
+    potencia_senial = mean(abs(mensaje).^2);
+    potencia_ruido = potencia_senial/(10^(snr_db/10));
     varianza = potencia_ruido/2;
     desviacion = sqrt(varianza);
     ruido_real = desviacion * randn(1,length(mensaje));
